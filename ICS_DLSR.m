c3bc155612ea2cb9aa9265c5b2a1487636b1adc3
Jie Wen <15990554+ckghostwj@users.noreply.github.com>
2018-06-13 10:41:43 +0800

    Code of paper Inter-class sparsity based discriminative least square regression
    Code of paper:
    Wen J, Xu Y, Li Z, et al. Inter-class sparsity based discriminative least square regression[J]. Neural Networks, 2018, 102: 36-47.

---
+++ ICS_DLSR.m
@@ -0,0 +1,53 @@
+function [Q,E,obj] = ICS_DLSR(X,Y,Train_Lab,opts)
+% code is written by Jie Wen
+% If any problems, please contact: wenjie@hrbeu.edu.cn
+% Please cite the reference:
+% Jie Wen, Yong Xu, Zuoyong Li, Zhongli Ma, Yuanrong Xu, 
+% Inter-class sparsity based discriminative least square regression [J],
+% Neural Networks, 2018, doi: 10.1016/j.neunet.2018.02.002.
+
+miu = opts.miu;
+rho = opts.rho;
+max_miu = 1e8;
+lambda1 = opts.lambda1;
+lambda2 = opts.lambda2;
+lambda3 = opts.lambda3;
+nnClass   = opts.nnClass;
+max_iter  = opts.maxIter;
+tol2 = 1e-2;
+Q  = rand(nnClass,size(X,1));
+E  = zeros(nnClass,size(X,2));
+F  = Q*X;
+C1 = zeros(size(F));
+for iter = 1:max_iter
+    % ----------- 更新Q ----------- %
+    G1 = Y+E;
+    G2 = F+C1/miu;
+    Q  = (G1+miu*G2)*X'*inv(X*X'*(1+miu)+lambda3*eye(size(X,1)));
+    % ---------- 更新 F ------------ %
+    H = Q*X-C1/miu;
+    F = [];
+    obj_F = 0;
+    for ic = 1:nnClass
+        idx = find(Train_Lab == ic);
+        H_ic = H(:,idx);
+        linshi_F = solve_l1l2(H_ic',lambda1/miu)';
+        F = [F,linshi_F];
+        obj_F = obj_F+sum(sqrt(sum(linshi_F.^2,2)));
+    end
+    clear linshi_F;
+    % ---------- 更新 E ----------- %
+    K = Q*X-Y;
+    E = solve_l1l2(K',lambda2)';
+    % ---------- C1 ------------ %
+    Lq = F-Q*X;
+    C1 = C1 + miu*Lq; 
+   
+    obj(iter) = 0.5*norm(Y+E-Q*X,'fro').^2+lambda3*0.5*norm(Q,'fro')^2+lambda1*obj_F+lambda2*sum(sqrt(sum(E.^2,2)));
+    if iter > 3 && abs(obj(iter)-obj(iter-1)) < 1e-7
+        iter
+        break;
+    end  
+end
+    
+    
---
+++ demo_ICS_DLSR.m
@@ -0,0 +1,73 @@
+% code is written by Jie Wen
+% If any problems, please contact: wenjie@hrbeu.edu.cn
+% Please cite the reference:
+% Jie Wen, Yong Xu, Zuoyong Li, Zhongli Ma, Yuanrong Xu, 
+% Inter-class sparsity based discriminative least square regression [J],
+% Neural Networks, 2018, doi: 10.1016/j.neunet.2018.02.002.
+
+clear all
+clc
+clear memory;
+
+% % name = 'YaleB_32x32'
+% % opts.lambda1 = 1e-2;
+% % opts.lambda2 = 2e-1;
+% % opts.lambda3 = 1e-2;
+% % sele_num = 10;
+
+
+name = 'LFW_1024_jerry';
+opts.lambda1 = 1e3;
+opts.lambda2 = 1e0;
+opts.lambda3 = 5e-2;
+sele_num = 8;
+
+% % name = 'COIL20'
+% % opts.lambda1 = 1e-4;
+% % opts.lambda2 = 1e-3;
+% % opts.lambda3 = 1e-2;
+% % sele_num = 25;
+
+load (name);
+fea = double(fea);
+nnClass = length(unique(gnd));  % The number of classes;
+num_Class = [];
+for i = 1:nnClass
+  num_Class = [num_Class length(find(gnd==i))]; %The number of samples of each class
+end
+%%------------------select training samples and test samples--------------%%
+Train_Ma  = [];
+Train_Lab = [];
+Test_Ma   = [];
+Test_Lab  = [];
+for j = 1:nnClass    
+    idx      = find(gnd==j);
+    randIdx  = randperm(num_Class(j));
+    Train_Ma = [Train_Ma; fea(idx(randIdx(1:sele_num)),:)];            % select select_num samples per class for training
+    Train_Lab= [Train_Lab;gnd(idx(randIdx(1:sele_num)))];
+    Test_Ma  = [Test_Ma;fea(idx(randIdx(sele_num+1:num_Class(j))),:)];  % select remaining samples per class for test
+    Test_Lab = [Test_Lab;gnd(idx(randIdx(sele_num+1:num_Class(j))))];
+end
+Train_Ma = Train_Ma';                       % transform to a sample per column
+Train_Ma = Train_Ma./repmat(sqrt(sum(Train_Ma.^2)),[size(Train_Ma,1) 1]);
+Test_Ma  = Test_Ma';
+Test_Ma  = Test_Ma./repmat(sqrt(sum(Test_Ma.^2)),[size(Test_Ma,1) 1]);  % -------------
+
+label = unique(Train_Lab);
+Y = bsxfun(@eq, Train_Lab, label');
+Y = double(Y)';
+
+opts.miu = 1e-8;
+opts.rho = 1.01;
+opts.nnClass = nnClass;
+opts.maxIter = 30;
+X = Train_Ma;
+
+[Q,E,obj] = ICS_DLSR(X,Y,Train_Lab,opts);
+% % figure;plot(obj);
+Train_Maa = Q*Train_Ma;
+Test_Maa  = Q*Test_Ma;
+Train_Maa = Train_Maa./repmat(sqrt(sum(Train_Maa.^2)),[size(Train_Maa,1) 1]);
+Test_Maa  = Test_Maa./repmat(sqrt(sum(Test_Maa.^2)),[size(Test_Maa,1) 1]);    
+[class_test] = knnclassify(Test_Maa', Train_Maa', Train_Lab,1,'euclidean','nearest');
+rate_acc = sum(Test_Lab == class_test)/length(Test_Lab)*100
